9e26234971d71093d6997418ebfc2fd13bd70767
Kevin Østerkilde <kevin@oesterkilde.dk>
2018-07-13 22:45:43 +0200

    Initial module

---
+++ index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import iconMap from './lib/iconMap'
+
+const Icon = props => {
+  return (
+    <svg
+			{...props}
+      width="100%"
+      height="100%"
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      aria-hidden="true"
+      dangerouslySetInnerHTML={{__html: iconMap[props.src]}}
+    />
+  )
+}
+
+module.exports = Icon
