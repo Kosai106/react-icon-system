bad01e2b1ce0ab1999704d4deb690525081b777a
Kevin Østerkilde <kevin@oesterkilde.dk>
2018-07-14 00:10:26 +0200

    Fix build

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
-import React from 'react';
-import iconMap from './lib/iconMap'
+import React, { Component } from "react";
+import iconMap from "./lib/iconMap";
 
-const Icon = props => {
-  return (
-    <svg
-      {...props}
-      width="100%"
-      height="100%"
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 24 24"
-      aria-hidden="true"
-      dangerouslySetInnerHTML={{__html: iconMap[props.src]}}
-    />
-  )
+class Icon extends Component {
+  render() {
+    return (
+      <svg
+        {...props}
+        width="100%"
+        height="100%"
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 24 24"
+        aria-hidden="true"
+        dangerouslySetInnerHTML={{ __html: iconMap[props.src] }}
+      />
+    );
+  }
 }
 
-export default Icon
+export default Icon;
--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,35 @@
-const path = require('path');
+const path = require("path");
+const webpack = require("webpack");
+const pkg = require("./package.json");
+const libraryName = pkg.name;
 
 module.exports = {
-  entry: './index.js',
+  entry: path.resolve(__dirname, "index.js"),
   output: {
-    filename: 'index.js',
-    libraryTarget: 'commonjs2'
+    path: path.resolve(__dirname, "build"),
+    filename: "index.js",
+    library: libraryName,
+    libraryTarget: "commonjs2"
   },
   module: {
     rules: [
       {
-				test: /\.svg$/,
-        loader: 'svg-inline-loader'
+        test: /\.js$/,
+        include: path.resolve(__dirname),
+        exclude: /(node_modules|build)/,
+        use: {
+          loader: "babel-loader"
+        }
       }
     ]
   },
+  resolve: {
+    alias: {
+      react: path.resolve(__dirname, "./node_modules/react"),
+      "react-dom": path.resolve(__dirname, "./node_modules/react-dom")
+    }
+  },
   externals: {
-    'react': 'commonjs react'
+    react: 'commonjs react'
   }
 };
