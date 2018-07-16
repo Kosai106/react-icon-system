29294fbce6b6e2d9267f12c8625bc5f54ad6dc29
Kevin Østerkilde <kevin@oesterkilde.dk>
2018-07-16 14:47:59 +0200

    Fixed props

--- index.js
+++ index.js
@@ -4,13 +4,13 @@ class Icon extends Component {
   render() {
     return (
       <svg
-        {...props}
+        {...this.props}
         width="100%"
         height="100%"
         xmlns="http://www.w3.org/2000/svg"
         viewBox="0 0 24 24"
         aria-hidden="true"
-        dangerouslySetInnerHTML={{ __html: props.src }}
+        dangerouslySetInnerHTML={{ __html: this.props.src }}
       />
     );
   }
