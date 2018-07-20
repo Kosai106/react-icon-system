23dfd85873407e418bcef8f47a930997f57a8d55
Kevin Østerkilde <kevin@oesterkilde.dk>
2018-07-21 01:18:48 +0200

    Converted to stateless component

--- Icon.js
+++ Icon.js
@@ -1,19 +1,15 @@
-import React, { Component } from "react";
+import React from "react";
 
-class Icon extends Component {
-  render() {
-    return (
-      <svg
-        {...this.props}
-        width="100%"
-        height="100%"
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 24 24"
-        aria-hidden="true"
-        dangerouslySetInnerHTML={{ __html: this.props.src }}
-      />
-    );
-  }
-}
+const Icon = props => (
+  <svg
+    {...props}
+    width="100%"
+    height="100%"
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 24 24"
+    aria-hidden="true"
+    dangerouslySetInnerHTML={{ __html: props.src }}
+  />
+)
 
 export default Icon;
