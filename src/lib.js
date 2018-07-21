2df02c38a5b2b56b9250b5f11ef403f244772aed
Kevin Østerkilde <kevin@oesterkilde.dk>
2018-07-21 17:45:29 +0200

    Updated paths

--- src/lib.js
+++ src/lib.js
@@ -1,8 +1,8 @@
-const fileList = require.context(process.env.REACT_APP_ICON_PATH, true, /[\s\S]*$/);
+const fileList = require.context(`../../../${process.env.REACT_APP_ICON_PATH}`, true, /[\s\S]*$/);
 let iconMap = {};
 fileList.keys().forEach(fileName => {
   fileName = fileName.replace('./', '');
-  iconMap[fileName.replace('.svg', '')] = require(`!svg-inline-loader!${process.env.REACT_APP_ICON_PATH}/${fileName}`);
+  iconMap[fileName.replace('.svg', '')] = require(`!svg-inline-loader!../../../${process.env.REACT_APP_ICON_PATH}/${fileName}`);
 });
 
 module.exports = iconMap
