031c5f586d3b6a3c165b79a4fcf48fabd6afdd4d
Kevin Østerkilde <kevin@oesterkilde.dk>
2018-07-21 18:04:35 +0200

    Fixed example code

--- example/src/App.js
+++ example/src/App.js
@@ -1,22 +1,22 @@
-import React, { Component } from 'react';
-import Icon from 'react-icon-system'
-import './App.css';
+import React, { Component } from "react";
+import Icon from "react-icon-system";
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <Icon src={'home'} className="App-logo" />
+          <Icon src="home" className="App-logo" />
           <h1 className="App-title">Welcome to React-Icon-System</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
 
-        <Icon src={'heart'} className="icon-preview color-green" />
-        <Icon src={'inbox'} className="icon-preview color-blue" />
-        <Icon src={'monitor'} className="icon-preview color-pink" />
+        <Icon src="heart" className="icon-preview color-green" />
+        <Icon src="inbox" className="icon-preview color-blue" />
+        <Icon src="monitor" className="icon-preview color-pink" />
       </div>
     );
   }
