02e1b7ba86ae4c471e0bfa2a9397eea7f967e102
Kevin Østerkilde <kevin@oesterkilde.dk>
2018-07-21 17:54:05 +0200

    Added example

--- example/src/App.js
+++ example/src/App.js
@@ -1,21 +1,22 @@
 import React, { Component } from 'react';
-import Icon, { iconMap } from 'react-icon-system/build';
-import logo from './logo.svg';
+import Icon from 'react-icon-system'
 import './App.css';
-
-const icons = iconMap('images/icons');
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <Icon src={'home'} className="App-logo" />
+          <h1 className="App-title">Welcome to React-Icon-System</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+
+        <Icon src={'heart'} className="icon-preview color-green" />
+        <Icon src={'inbox'} className="icon-preview color-blue" />
+        <Icon src={'monitor'} className="icon-preview color-pink" />
       </div>
     );
   }
