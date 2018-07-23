5c683effb2fa315c0515c916fd000d4b4204859b
Kevin Østerkilde <kevin@oesterkilde.dk>
2018-07-23 16:08:34 +0200

    Moved ...props to allow for easier sizing

--- src/index.js
+++ src/index.js
@@ -2,13 +2,13 @@ import React from "react";
 import iconMap from "./lib";
 
 const Icon = props => (
-  <svg
-    {...props}
+  <svg 
     width="100%"
     height="100%"
     xmlns="http://www.w3.org/2000/svg"
     viewBox="0 0 24 24"
     aria-hidden="true"
+    {...props}
     dangerouslySetInnerHTML={{ __html: iconMap[props.src] }}
   />
 )
