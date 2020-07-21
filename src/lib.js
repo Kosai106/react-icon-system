78db6def8208dd9b05d1a27eb628358864b3525a
Kevin Østerkilde <kevin@oesterkilde.dk>
2020-07-21 17:44:03 +0200

    feat(lib): convert to typescript

--- src/lib.js
+++
@@ -1,8 +0,0 @@
-const fileList = require.context(`../../../${process.env.REACT_APP_ICON_PATH}`, true, /[\s\S]*$/);
-let iconMap = {};
-fileList.keys().forEach(fileName => {
-  fileName = fileName.replace('./', '');
-  iconMap[fileName.replace('.svg', '')] = require(`!svg-inline-loader!../../../${process.env.REACT_APP_ICON_PATH}/${fileName}`);
-});
-
-module.exports = iconMap
